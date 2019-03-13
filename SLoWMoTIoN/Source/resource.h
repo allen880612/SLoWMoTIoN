850c82952641a3c2ce363723201bc21dad356bd0
allen880612 <allen880612@gmail.com>
2019-03-14 02:33:15 +0800

    加油

--- SLoWMoTIoN/Source/resource.h
+++ SLoWMoTIoN/Source/resource.h
@@ -43,17 +43,10 @@
 #define ID_GREEN                        161
 #define ID_GREEN1                       162
 #define IDB_BITMAP4                     162
-<<<<<<< HEAD
 #define IDB_MAP0                        165
 #define IDB_MAP1                        166
 #define IDB_BITMAP7                     167
 #define IDB_MAP2                        167
-=======
-#define IDB_MAP0                        163
-#define IDB_MAP1                        164
-#define IDB_BITMAP7                     165
-#define IDB_MAP2                        165
->>>>>>> 1ad9adbb077f17d2a8673e943faf49131db44627
 #define IDC_README                      1001
 #define ID_FILE_PAUSE                   32771
 #define ID_TOGGLE_FULLSCREEN            32772
@@ -67,11 +60,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
-<<<<<<< HEAD
 #define _APS_NEXT_RESOURCE_VALUE        169
-=======
-#define _APS_NEXT_RESOURCE_VALUE        166
->>>>>>> 1ad9adbb077f17d2a8673e943faf49131db44627
 #define _APS_NEXT_COMMAND_VALUE         32776
 #define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           102
