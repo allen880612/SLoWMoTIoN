1ad9adbb077f17d2a8673e943faf49131db44627
Raven9527 <acd667y@gmail.com>
2019-03-14 02:18:42 +0800

    Revert "最好抓的到"
    This reverts commit 1f0c2fb675a86167c4a9c2c5550b5c3bb6fa870e.

--- SLoWMoTIoN/Source/resource.h
+++ SLoWMoTIoN/Source/resource.h
@@ -43,10 +43,10 @@
 #define ID_GREEN                        161
 #define ID_GREEN1                       162
 #define IDB_BITMAP4                     162
-#define IDB_MAP0                        166
-#define IDB_MAP1                        167
-#define IDB_BITMAP7                     168
-#define IDB_MAP2                        168
+#define IDB_MAP0                        163
+#define IDB_MAP1                        164
+#define IDB_BITMAP7                     165
+#define IDB_MAP2                        165
 #define IDC_README                      1001
 #define ID_FILE_PAUSE                   32771
 #define ID_TOGGLE_FULLSCREEN            32772
@@ -60,7 +60,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
-#define _APS_NEXT_RESOURCE_VALUE        169
+#define _APS_NEXT_RESOURCE_VALUE        166
 #define _APS_NEXT_COMMAND_VALUE         32776
 #define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           101
